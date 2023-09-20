9b416501e881ef5d584d576218e485074fcc331f
Youssef Elsharkawy <41446666+YoussefElsharkawy55@users.noreply.github.com>
2023-09-21 00:57:32 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
-void prints_numbers(int n)
+void print_numbers(int n)
 {
 	unsigned int n1;
-	n1=n;
+	n1 = n;
+	
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
-	}	
+	}
+
 	if (n1 / 10 != 0)
 	{
-		prints_numbers(n1 / 10);
+		print_numbers(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
