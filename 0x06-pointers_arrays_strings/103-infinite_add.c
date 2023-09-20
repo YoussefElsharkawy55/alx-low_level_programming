5e6859ebf0f00e694c2434c7b62efab14ce8e575
Youssef Elsharkawy <41446666+YoussefElsharkawy55@users.noreply.github.com>
2023-09-21 01:00:01 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
-char *infinite_add(char *n1,char *n2, char *r int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i =0, j= 0, k , l = 0, f, s , d = 0;
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	while(n1[i] != '\0')
+	while (n1[i] != '\0')
 		i++;
-	while(n2[j] != '\0')
+	while (n2[j] != '\0')
 		j++;
+
 	if (i > j)
-		l =i;
+		l = i;
 	else
 		l = j;
+
 	if (l + 1 > size_r)
 		return (0);
 
 	r[l] = '\0';
-	for(k = l - 1; k >= 0; k--)
+
+	for (k = l - 1; k >= 0; k--)
 	{
 		i--;
 		j--;
@@ -31,16 +34,16 @@ char *infinite_add(char *n1,char *n2, char *r int size_r)
 		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
 	}
-	if(d==1)
+
+	if (d == 1)
 	{
 		r[l + 1] = '\0';
-		if(l+ 2 > size_r)
+		if (l + 2 > size_r)
 			return (0);
-		while(l-- >= 0)
+		while (l-- >= 0)
 			r[l + 1] = r[l];
 		r[0] = d + '0';
-	
 	}
-return (r);
 
+	return (r);
 }
