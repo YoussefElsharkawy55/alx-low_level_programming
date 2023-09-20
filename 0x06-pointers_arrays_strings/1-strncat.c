a43887c2ab866f32a75dafd1c93ca118176250dc
Youssef <youssef.elshakrawy2001@gmail.com>
2023-09-20 14:27:42 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, const char *src, int n)
 {
 	int c, i;
-	c =0;
-	while(dest[c])
-	{
+
+	c = 0;
+
+	while (dest[c])
 		c++;
-	}
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[c + i] = src[i];
 	}
+
 	dest[c + i] = '\0';
+
 	return (dest);
 }
