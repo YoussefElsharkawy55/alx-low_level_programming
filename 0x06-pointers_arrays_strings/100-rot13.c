33fef5a5626b115cf9b79ef55f24ea2500a73988
Youssef Elsharkawy <41446666+YoussefElsharkawy55@users.noreply.github.com>
2023-09-21 00:55:34 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,44 @@
 #include "main.h"
+#include <stdlib.h>
 
-
-char *rot13(char *s)
+char *rot13(const char *s)
 {
-	int i;
-	char rot13[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[]="nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *ptr =s;
+    int i;
+    char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+    char *result = malloc(strlen(s) + 1);
 
+    if (result == NULL) {
+        return NULL;
+    }
 
-	while (*s)
-	{
-		for (i = 0; i < 52; i++)
-		{
-			if (*s == rot13[i])
-			{
-				*s = ROT13[i];
-				break;
-			}
-		}
-		s++;
-	}
-	return (ptr);
+    char *dest = result;
+
+    while (*s)
+    {
+        char c = *s;
+        char found = 0;
+
+        for (i = 0; i < 52; i++)
+        {
+            if (c == rot13[i])
+            {
+                *dest = ROT13[i];
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            *dest = c;
+        }
+
+        s++;
+        dest++;
+    }
+
+    *dest = '\0';
+
+    return (result);
 }
-	
