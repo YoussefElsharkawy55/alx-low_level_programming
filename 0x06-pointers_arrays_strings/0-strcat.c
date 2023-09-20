cc159b6bdb267e7ea0cb244df5cc9495ad978b9e
Youssef <youssef.elshakrawy2001@gmail.com>
2023-09-20 14:21:00 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
-    int c , c2;
-    c= 0;
-    while (dest[c] )
-    {
-        c++;
-    }   
-    for (c2 = 0; src[c2]; c2++)
-    {
-        dest[c++] = src[c2];
-    
-    }
-    retrurn (dest);
+	int c, c2;
 
+	for (c = 0; dest[c]; c++)
+	{
+	}
+
+	for (c2 = 0; src[c2]; c2++)
+	{
+		dest[c + c2] = src[c2];
+	}
+
+	dest[c + c2] = '\0';
+
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,17 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c) ;
-char *_strcat(char *dest, char *src) ;
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *si, char *s2);
+
+int _putchar(char c);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, int n);
+char *_strncpy(char *dest, const char *src, int n);
+int _strcmp(const char *s1, const char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *) ;
-char *cap string(char * ) ;
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
 void print_number(int n);
-char *infinite_add(char *ni, char *n2, char *r ,int size_r);
+char *infinite_add(const char *n1, const char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
+
