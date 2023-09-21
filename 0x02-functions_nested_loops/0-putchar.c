8aec1c4d3c4453d6a180b3b71da3f9d56417e792
Youssef Elsharkawy <41446666+YoussefElsharkawy55@users.noreply.github.com>
2023-09-21 18:45:19 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 int main(void)
 {
-    char str[] = "_putchar";
-    int ch;
-    for (ch = 0; ch < 8; ch++)
-        _putchar(str[ch]);
-    _putchar('\n');
-    return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+
+	_putchar('\n');
+
+	return (0);
 }
